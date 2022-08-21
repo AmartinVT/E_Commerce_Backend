d86f59a925855e2145ec7ebb2ec889a4e87523d0
VT_AOE_91 <amartin80108@gmail.com>
2022-08-21 19:04:22 -0400

    Fixed delete variable name error

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -125,10 +125,10 @@ router.put('/:id', async (req, res) => {
 router.delete('/:id', async (req, res) => {
   // delete one product by its `id` value
   try{
-    const data = await Product.destroy({
+    const productData = await Product.destroy({
       where: {id: req.params.id}
     });
-    res.status(200).json(data);
+    res.status(200).json(productData);
   } catch (err) {
     res.status(500).json(err);
   }
