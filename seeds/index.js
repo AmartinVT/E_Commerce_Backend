d7fb94d6663ae0bd596748e757229d50149e0a32
VT_AOE_91 <amartin80108@gmail.com>
2022-08-21 14:18:42 -0400

    Added sequelize statement to seed index file

--- seeds/index.js
+++ seeds/index.js
@@ -7,8 +7,9 @@ const seedProductTags = require('./product-tag-seeds');
 const sequelize = require('../config/connection');
 
 const seedAll = async () => {
-  await sync({ force: true });
+  await sequelize.sync({ force: true });
   console.log('\n----- DATABASE SYNCED -----\n');
+  
   await seedCategories();
   console.log('\n----- CATEGORIES SEEDED -----\n');
 
