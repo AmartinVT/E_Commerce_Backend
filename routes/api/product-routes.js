eee3459460cf4032ac47f1166b6bb8a28dde9353
VT_AOE_91 <amartin80108@gmail.com>
2022-08-21 16:25:29 -0400

    Fixed product-routes.js get request to include attributes

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -1,5 +1,5 @@
 const router = require('express').Router();
-const { Category, Product } = require('../../models');
+const { Product, Category, Tag, ProductTag } = require('../../models');
 
 // The `/api/products` endpoint
 
@@ -8,12 +8,15 @@ router.get('/', (req, res) => {
   // find all products
   // be sure to include its associated Category and Tag data
   try {
-    const productData = Product.findAll({
-      include: 
-      [
-        {model: Category, through: category_id, as: 'product_category'},
-        {model: Tag}
-      ]
+    const productData = await Product.findAll({
+      attributes: ['id', 'product_name', 'price', 'stock', 'category_id'],
+      include: [{
+        model: Category,
+        attributes: ['id', 'category_name']
+      },{
+        model: Tag,
+        attributes: ['id', 'tag_name']
+      }]
     });
     res.status(200).json(categoryData);
   } catch (err) {
@@ -25,7 +28,7 @@ router.get('/', (req, res) => {
 router.get('/:id', (req, res) => {
   // find a single product by its `id`
   // be sure to include its associated Category and Tag data
-  Category.findOne({
+  await Category.findOne({
     where: {
       id: req.params.id
     },
